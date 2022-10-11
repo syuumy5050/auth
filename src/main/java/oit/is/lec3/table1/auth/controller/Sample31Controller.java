38865aecd96d9a3e703e46158d92929ecc17cf28
e1b20062T <e1b20062@oit.ac.jp>
2022-10-11 15:33:45 +0900

    sample33.htmlを表示するGETリクエストを実装する

--- src/main/java/oit/is/lec3/table1/auth/controller/Sample31Controller.java
+++ src/main/java/oit/is/lec3/table1/auth/controller/Sample31Controller.java
@@ -10,19 +10,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 @RequestMapping("/sample3")
 public class Sample31Controller {
-    @GetMapping("step1")
-    public String sample31() {
-        return "sample31.html";
-    }
+  @GetMapping("step1")
+  public String sample31() {
+    return "sample31.html";
+  }
 
-    @GetMapping("step2")
-    public String sample32(ModelMap model, Principal prin) {
-      String loginUser = prin.getName(); // ログインユーザ情報
-      model.addAttribute("login_user", loginUser);
-      return "sample31.html";
-    }
+  @GetMapping("step2")
+  public String sample32(ModelMap model, Principal prin) {
+    String loginUser = prin.getName(); // ログインユーザ情報
+    model.addAttribute("login_user", loginUser);
+    return "sample31.html";
+  }
+
+  @GetMapping("step3")
+  public String sample33() {
+    return "sample33.html";
+  }
 }
