1be996d99fa8547d470472c7d8296f34b8084ad2
syuumy5050 <osamigaki3856@gmail.com>
2022-10-11 14:24:47 +0900

    レポジトリの初期化

---
+++ src/main/java/oit/is/lec3/table1/auth/AuthApplication.java
@@ -0,0 +1,13 @@
+package oit.is.lec3.table1.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/lec3/table1/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.lec3.table1.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
