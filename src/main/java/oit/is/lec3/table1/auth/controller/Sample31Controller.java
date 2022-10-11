c63ff781fe7f15859b4e13a8e57dedb957e33581
syuumy5050 <osamigaki3856@gmail.com>
2022-10-11 16:11:06 +0900

    入室処理をDIとnew2種類の方法で実装する

--- src/main/java/oit/is/lec3/table1/auth/controller/Sample31Controller.java
+++ src/main/java/oit/is/lec3/table1/auth/controller/Sample31Controller.java
@@ -10,9 +10,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import oit.is.lec3.table1.auth.model.Room;
+
 @Controller
 @RequestMapping("/sample3")
 public class Sample31Controller {
+
+  @Autowired
+  private Room room;
+
   @GetMapping("step1")
   public String sample31() {
     return "sample31.html";
@@ -39,6 +45,25 @@ public class Sample31Controller {
 
   @GetMapping("step7")
   public String sample37() {
+    return "sample37.html";
+  }
+
+  @GetMapping("step8")
+  public String sample38(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.room.addUser(loginUser);
+    model.addAttribute("room", this.room);
+
+    return "sample37.html";
+  }
+
+  @GetMapping("step9")
+  public String sample39(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    Room newRoom = new Room();
+    newRoom.addUser(loginUser);
+    model.addAttribute("new_room", newRoom);
+
     return "sample37.html";
   }
 }
