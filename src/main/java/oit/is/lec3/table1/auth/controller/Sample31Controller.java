6a8ac3cb49b22e074f6de2f326963e097eac8e4d
e1b20014 <e1b20014@oit.ac.jp>
2022-10-11 15:21:39 +0900

    sample31.htmlを表示するGETリクエストを実装する

---
+++ src/main/java/oit/is/lec3/table1/auth/controller/Sample31Controller.java
@@ -0,0 +1,23 @@
+package oit.is.lec3.table1.auth.controller;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/sample3")
+public class Sample31Controller {
+    @GetMapping("step1")
+    public String sample31() {
+        return "sample31.html";
+    }
+
+
+}
